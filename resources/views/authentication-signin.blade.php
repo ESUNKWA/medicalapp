7b79fbf28babd1933afb2387d6a2097105fcbf4b
ESUNKWA <kouadiodeki@gmail.com>
2022-10-23 09:21:55 +0000

    login

--- resources/views/authentication-signin.blade.php
+++ resources/views/authentication-signin.blade.php
@@ -15,7 +15,7 @@
 	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet">
 		<link href="assets/css/app.css" rel="stylesheet">
 		<link href="assets/css/icons.css" rel="stylesheet">
-		<title>Rocker - Multipurpose Bootstrap5 Admin Template</title>
+		<title>SIGMed</title>
 	</head>
 
 <body class="bg-login">
@@ -25,43 +25,41 @@
             <div class="container-fluid">
                 <div class="row row-cols-1 row-cols-lg-2 row-cols-xl-3">
                     <div class="col mx-auto">
-                        <div class="mb-4 text-center">
+                        {{-- <div class="mb-4 text-center">
                             <img src="assets/images/logo-img.png" width="180" alt="" />
-                        </div>
+                        </div> --}}
                         <div class="card">
                             <div class="card-body">
                                 <div class="border p-4 rounded">
                                     <div class="text-center">
-                                        <h3 class="">Sign in</h3>
-                                        <p>Don't have an account yet? <a href="{{ url('authentication-signup') }}">Sign up here</a>
-                                        </p>
+                                        <h3 class="">Se connecter</h3>
                                     </div>
-                                    <div class="login-separater text-center mb-4"> <span>OR SIGN IN WITH EMAIL</span>
+                                    <div class="login-separater text-center mb-4"> <span>Connecter vous avec vos accès</span>
                                         <hr/>
                                     </div>
                                     <div class="form-body">
                                         <form class="row g-3" method="get" action="index" >
                                             <div class="col-12">
-                                                <label for="inputEmailAddress" class="form-label">Email Address</label>
-                                                <input type="email" class="form-control" id="inputEmailAddress" placeholder="Email Address">
+                                                <label for="inputEmailAddress" class="form-label">Identifiant</label>
+                                                <input type="email" class="form-control" id="inputEmailAddress" placeholder="">
                                             </div>
                                             <div class="col-12">
-                                                <label for="inputChoosePassword" class="form-label">Enter Password</label>
+                                                <label for="inputChoosePassword" class="form-label">Mot de passe</label>
                                                 <div class="input-group" id="show_hide_password">
-                                                    <input type="password" class="form-control border-end-0" id="inputChoosePassword" value="12345678" placeholder="Enter Password"> <a href="javascript:;" class="input-group-text bg-transparent"><i class='bx bx-hide'></i></a>
+                                                    <input type="password" class="form-control border-end-0" id="inputChoosePassword" value="" placeholder=""> <a href="javascript:;" class="input-group-text bg-transparent"><i class='bx bx-hide'></i></a>
                                                 </div>
                                             </div>
                                             <div class="col-md-6">
                                                 <div class="form-check form-switch">
                                                     <input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked" checked>
-                                                    <label class="form-check-label" for="flexSwitchCheckChecked">Remember Me</label>
+                                                    <label class="form-check-label" for="flexSwitchCheckChecked">Se souvenir de moi</label>
                                                 </div>
                                             </div>
-                                            <div class="col-md-6 text-end">	<a href="{{ url('authentication-forgot-password') }}">Forgot Password ?</a>
+                                             <div class="col-md-6 text-end">	<a href="{{ url('authentication-forgot-password') }}"> Mot de passe oubliez ?</a>
                                             </div>
                                             <div class="col-12">
                                                 <div class="d-grid">
-                                                    <button type="submit" class="btn btn-primary"><i class="bx bxs-lock-open"></i>Sign in</button>
+                                                    <button type="submit" class="btn btn-primary"><i class="bx bxs-lock-open"></i>Connexion</button>
                                                 </div>
                                             </div>
                                         </form>
